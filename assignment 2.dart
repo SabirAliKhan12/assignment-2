62305ddf1ddee7d50e6c05dbf8d220eb9c8cf22b
SabirAliKhan12 <101522321+SabirAliKhan12@users.noreply.github.com>
2022-03-13 11:36:43 -0700

    assignment 2

---
+++ assignment 2.dart
@@ -0,0 +1,21 @@
+main() {
+//coversation of int into string
+  int age = 45;
+  String Ali_age = age.toString();
+  print(Ali_age);
+
+//conversation of string into int
+  String temp = '55';
+  int tempint = int.parse(temp);
+  print(tempint);
+
+//conversation of double to string
+  double result = 90.5;
+  String resultText = result.toString();
+  print(resultText);
+
+//conversation of string to double
+  String Boys = '55';
+  double totalBoys = double.parse(Boys);
+  print(totalBoys);
+}
